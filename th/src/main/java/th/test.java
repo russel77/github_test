f7524c17c3224ab426f2fb31921efe0114373120
russel77 <97882707+russel77@users.noreply.github.com>
2022-03-02 12:01:05 +0000

    add

---
+++ th/src/main/java/th/test.java
@@ -0,0 +1,36 @@
+package th;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class test {
+    @Test
+    void  se(){System.setProperty("webdriver.chrome.driver","C:\\Users\\russe\\Downloads\\chromedriver_win32 (1)\\chromedriver.exe");
+    WebDriver driver = new ChromeDriver();
+    driver.get("https://status.vodafonebusiness.ringcentral.com/");
+    driver.manage().window().maximize();
+        WebDriver.Timeouts time = driver.manage().timeouts();
+        time.implicitlyWait(60L, TimeUnit.SECONDS);
+    if (driver.getPageSource().contains("class=\"have-info-wrapper left large CId-incident-icon IId-incident-icon board-error")) {
+        System.out.println(" Error found ");
+    } else {
+        System.out.println(" No issues Found");
+    }
+        if (driver.getPageSource().contains("class=\"have-info-wrapper left large CId-incident-icon IId-incident-icon board-information")) {
+            System.out.println(" Information Notice ");
+        } else {
+            System.out.println(" No issues Found");
+        }
+        if (driver.getPageSource().contains("class=\"have-info-wrapper left large CId-incident-icon IId-incident-icon board-warn")) {
+            System.out.println(" issues found ");
+        } else {
+            System.out.println(" No issues Found");
+        }
+
+}
+}
